7223f6adea0c614f50769da02874688527798b7f
beshirD <6beshir@123github>
2023-09-07 15:46:39 +0300

    clean up file upload

--- src/components/file-upload/index.tsx
+++ src/components/file-upload/index.tsx
@@ -1,18 +1,6 @@
 // components/FileUpload.js
 import { useState } from 'react';
 import axios from 'axios';
-import { createClient } from '@supabase/supabase-js';
-
-import { SupabaseVectorStore } from 'langchain/vectorstores/supabase';
-import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
-
-import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
-// import * as fs from 'fs';
-
-// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_BASE_URL;
-// console.log(supabaseUrl,"supabase url")
-// const supabaseServiceKey = process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY;
-// const supabase = createClient(supabaseUrl, supabaseServiceKey);
 
 const FileUpload = () => {
   const [selectedFile, setSelectedFile] = useState(null);
@@ -22,49 +10,6 @@ const FileUpload = () => {
     setSelectedFile(e.target.files[0]);
   };
   
-  // const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY;
-  // if (!supabaseKey) throw new Error(`Expected SUPABASE_SERVICE_ROLE_KEY`);
-  
-  // const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
-  // if (!url) throw new Error(`Expected env var SUPABASE_URL`);
-  // const openaikey = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
-  // const client = createClient(url, supabaseKey);
-
-  const createEmbeddings = async (input:any) => {
-    // const fileBuffer = input.buffer;
-    console.log(input,"the input")
-    const text = input?.buffer?.toString('utf-8');
-    // const text = fs.readFileSync('pizzareview.txt', 'utf8');
-    const textSplitter = new RecursiveCharacterTextSplitter({
-      chunkSize: 200,
-      chunkOverlap: 50,
-    });
-    const docs = await textSplitter.createDocuments([text]);
-    // const vectorStore = await SupabaseVectorStore.fromDocuments(docs, new OpenAIEmbeddings(), {
-    //   client,
-    //   tableName: 'documents',
-    //   queryName: 'match_documents',
-    // });
-    console.log(docs,"the docs things")
-  };
-
-  const run = async () => {
-    //the upload part
-    const vectorStore = await SupabaseVectorStore.fromTexts(
-      ['Hello world', 'Bye bye', "What's this?"],
-      [{ id: 2 }, { id: 1 }, { id: 3 }],
-      new OpenAIEmbeddings({ openAIApiKey: openaikey }),
-      {
-        client,
-        tableName: 'documents',
-        queryName: 'match_documents',
-      },
-    );
-    console.log('ezi dersual');
-    const resultOne = await vectorStore.similaritySearch('Hello');
-
-    console.log(resultOne, 'the result is here');
-  };
 
   const handleUpload = async () => {
     if (!selectedFile) return;
@@ -72,14 +17,12 @@ const FileUpload = () => {
     const formData = new FormData();
     formData.append('file', selectedFile);
     console.log(formData,"the form data");
-    // createEmbeddings(formData)
     try {
       await axios.post('/api/testupload/embedding', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
       });
-      // run();
       console.log('File uploaded successfully');
     } catch (error) {
       console.error('Error uploading file:', error);
@@ -87,12 +30,7 @@ const FileUpload = () => {
   };
   const handleSearch = async () => {
     console.log(question, 'the query here');
-    // let { data, error } = await supabase.rpc('search_ai_data', {
-    //   question,
-    // });
 
-    // if (error) console.error(error, 'the error');
-    // else console.log(data, 'the answer');
     try {
       const reqeustBody ={query:question}
       const response =  await axios.post('/api/testupload/search', reqeustBody);
